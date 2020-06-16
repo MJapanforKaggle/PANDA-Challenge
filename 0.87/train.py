0e9a7c9361c8084b3f73b1f76355d9720813c850
Reo Hayashi <43193985+bybrooks@users.noreply.github.com>
2020-06-16 18:31:25 +0900

    Create train.py

---
+++ 0.87/train.py
@@ -0,0 +1,281 @@
+import os
+import sys
+import time
+import numpy as np
+import pandas as pd
+import cv2
+import PIL.Image
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.sampler import SubsetRandomSampler, RandomSampler, SequentialSampler
+import pytorch_gradual_warmup_lr.warmup_scheduler 
+import EfficientNet_PyTorch.efficientnet_pytorch.model as enet
+import albumentations
+from sklearn.model_selection import StratifiedKFold
+import matplotlib.pyplot as plt
+from sklearn.metrics import cohen_kappa_score
+from tqdm import tqdm_notebook as tqdm
+
+DEBUG = True
+data_dir = 'train/'
+df_train = pd.read_csv('train.csv')
+image_folder = os.path.join(data_dir)
+
+#kernel_type = 'how_to_train_effnet_b0_to_get_LB_0.86'
+
+enet_type = 'efficientnet-b0'
+fold = 0
+tile_size = 256
+image_size = 256
+n_tiles = 36
+batch_size = 2
+num_workers = 4
+out_dim = 5
+init_lr = 3e-4
+warmup_factor = 10
+
+warmup_epo = 1
+n_epochs = 1 if DEBUG else 30
+df_train = df_train.sample(100).reset_index(drop=True) if DEBUG else df_train
+
+device = torch.device('cuda')
+
+
+skf = StratifiedKFold(5, shuffle=True, random_state=42)
+df_train['fold'] = -1
+for i, (train_idx, valid_idx) in enumerate(skf.split(df_train, df_train['isup_grade'])):
+    df_train.loc[valid_idx, 'fold'] = i
+df_train.head()
+
+pretrained_model = {
+    'efficientnet-b0': 'efficientnet-b0-08094119.pth'
+}
+
+class enetv2(nn.Module):
+    def __init__(self, backbone, out_dim):
+        super(enetv2, self).__init__()
+        self.enet = enet.EfficientNet.from_name(backbone)
+        self.enet.load_state_dict(torch.load(pretrained_model[backbone]))
+
+        self.myfc = nn.Linear(self.enet._fc.in_features, out_dim)
+        self.enet._fc = nn.Identity()
+
+    def extract(self, x):
+        return self.enet(x)
+
+    def forward(self, x):
+        x = self.extract(x)
+        x = self.myfc(x)
+        return x
+
+def get_tiles(img, mode=0):
+    # input image shape is 36 x 3 x 256 x 256
+    result = []
+    grayls = np.zeros((36, 1, 256, 256, ))
+    #img4 = img.transpose(0,3,2,1)
+
+    for i in range(36):
+        lap = cv2.Laplacian(img[i], cv2.CV_32F,ksize=5)  # 3 x 256 x 256
+        lap = lap.transpose(1,2,0)
+        gray = cv2.cvtColor(lap, cv2.COLOR_BGR2GRAY)   # 256 x 256
+        gray = gray[np.newaxis, : , : ]
+        grayls[i] = gray
+
+    img5 = np.append(img, grayls, axis=1)
+    img5 = img5.transpose(0, 2, 3, 1)
+
+    for i in range(len(img5)):
+        result.append({'img':img5[i], 'idx':i})
+    return result
+
+
+class PANDADataset(Dataset):
+    def __init__(self,
+                 df,
+                 image_size,
+                 n_tiles=n_tiles,
+                 tile_mode=0,
+                 rand=False,
+                 transform=None,
+                ):
+
+        self.df = df.reset_index(drop=True)
+        self.image_size = image_size
+        self.n_tiles = n_tiles
+        self.tile_mode = tile_mode
+        self.rand = rand
+        self.transform = transform
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, index):
+        image = np.zeros((36, 3, 256, 256))
+        row = self.df.iloc[index]       # 
+        img_id = row.image_id
+        
+        
+        for i in range(36):
+            png_file = os.path.join(image_folder, f'{img_id}','_',f'{i}','.png')
+            tile_get = cv2.imread(png_file)
+            image[i] = tile_get 
+        tiles = get_tiles(image, self.tile_mode)
+
+        if self.rand:
+            idxes = np.random.choice(list(range(self.n_tiles)), self.n_tiles, replace=False)
+        else:
+            idxes = list(range(self.n_tiles))
+
+        n_row_tiles = int(np.sqrt(self.n_tiles))
+        images = np.zeros((image_size * n_row_tiles, image_size * n_row_tiles, 4))
+        for h in range(n_row_tiles):
+            for w in range(n_row_tiles):
+                i = h * n_row_tiles + w
+    
+                if len(tiles) > idxes[i]:
+                    this_img = tiles[idxes[i]]['img']
+                else:
+                    this_img = np.ones((self.image_size, self.image_size, 4)).astype(np.uint8) * 255
+                this_img = 255 - this_img
+                if self.transform is not None:
+                    this_img = self.transform(image=this_img)['image']
+                h1 = h * image_size
+                w1 = w * image_size
+                images[h1:h1+image_size, w1:w1+image_size] = this_img
+
+        if self.transform is not None:
+            images = self.transform(image=images)['image']
+        images = images.astype(np.float32)
+        images /= 255
+        images = images.transpose(2, 0, 1)
+
+        label = np.zeros(5).astype(np.float32)
+        label[:row.isup_grade] = 1.
+        return torch.tensor(images), torch.tensor(label)
+
+transforms_train = albumentations.Compose([
+    albumentations.Transpose(p=0.5),
+    albumentations.VerticalFlip(p=0.5),
+    albumentations.HorizontalFlip(p=0.5),
+])
+transforms_val = albumentations.Compose([])
+
+dataset_show = PANDADataset(df_train, image_size, n_tiles, 0, transform=transforms_train)
+from pylab import rcParams
+rcParams['figure.figsize'] = 20,10
+for i in range(2):
+    f, axarr = plt.subplots(1,5)
+    for p in range(5):
+        idx = np.random.randint(0, len(dataset_show))
+        img, label = dataset_show[idx]
+        axarr[p].imshow(1. - img.transpose(0, 1).transpose(1,2).squeeze())
+        axarr[p].set_title(str(sum(label)))
+
+criterion = nn.BCEWithLogitsLoss()
+
+def train_epoch(loader, optimizer):
+
+    model.train()
+    train_loss = []
+    bar = tqdm(loader)
+    for (data, target) in bar:
+        
+        data, target = data.to(device), target.to(device)
+        loss_func = criterion
+        optimizer.zero_grad()
+        logits = model(data)
+        loss = loss_func(logits, target)
+        loss.backward()
+        optimizer.step()
+
+        loss_np = loss.detach().cpu().numpy()
+        train_loss.append(loss_np)
+        smooth_loss = sum(train_loss[-100:]) / min(len(train_loss), 100)
+        bar.set_description('loss: %.5f, smth: %.5f' % (loss_np, smooth_loss))
+    return train_loss
+
+
+def val_epoch(loader, get_output=False):
+
+    model.eval()
+    val_loss = []
+    LOGITS = []
+    PREDS = []
+    TARGETS = []
+
+    with torch.no_grad():
+        for (data, target) in tqdm(loader):
+            data, target = data.to(device), target.to(device)
+            logits = model(data)
+
+            loss = criterion(logits, target)
+
+            pred = logits.sigmoid().sum(1).detach().round()
+            LOGITS.append(logits)
+            PREDS.append(pred)
+            TARGETS.append(target.sum(1))
+
+            val_loss.append(loss.detach().cpu().numpy())
+        val_loss = np.mean(val_loss)
+
+    LOGITS = torch.cat(LOGITS).cpu().numpy()
+    PREDS = torch.cat(PREDS).cpu().numpy()
+    TARGETS = torch.cat(TARGETS).cpu().numpy()
+    acc = (PREDS == TARGETS).mean() * 100.
+    
+    qwk = cohen_kappa_score(PREDS, TARGETS, weights='quadratic')
+    qwk_k = cohen_kappa_score(PREDS[df_valid['data_provider'] == 'karolinska'], df_valid[df_valid['data_provider'] == 'karolinska'].isup_grade.values, weights='quadratic')
+    qwk_r = cohen_kappa_score(PREDS[df_valid['data_provider'] == 'radboud'], df_valid[df_valid['data_provider'] == 'radboud'].isup_grade.values, weights='quadratic')
+    print('qwk', qwk, 'qwk_k', qwk_k, 'qwk_r', qwk_r)
+
+    if get_output:
+        return LOGITS
+    else:
+        return val_loss, acc, qwk
+
+train_idx = np.where((df_train['fold'] != fold))[0]
+valid_idx = np.where((df_train['fold'] == fold))[0]
+
+df_this  = df_train.loc[train_idx]
+df_valid = df_train.loc[valid_idx]
+
+dataset_train = PANDADataset(df_this , image_size, n_tiles, transform=transforms_train)
+dataset_valid = PANDADataset(df_valid, image_size, n_tiles, transform=transforms_val)
+
+train_loader = torch.utils.data.DataLoader(dataset_train, batch_size=batch_size, sampler=RandomSampler(dataset_train), num_workers=num_workers)
+valid_loader = torch.utils.data.DataLoader(dataset_valid, batch_size=batch_size, sampler=SequentialSampler(dataset_valid), num_workers=num_workers)
+
+model = enetv2(enet_type, out_dim=out_dim)
+model = model.to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=init_lr/warmup_factor)
+scheduler_cosine = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, n_epochs-warmup_epo)
+scheduler = GradualWarmupScheduler(optimizer, multiplier=warmup_factor, total_epoch=warmup_epo, after_scheduler=scheduler_cosine)
+
+print(len(dataset_train), len(dataset_valid))
+
+qwk_max = 0.
+best_file = f'{kernel_type}_best_fold{fold}.pth'
+for epoch in range(1, n_epochs+1):
+    print(time.ctime(), 'Epoch:', epoch)
+    scheduler.step(epoch-1)
+
+    train_loss = train_epoch(train_loader, optimizer)
+    val_loss, acc, qwk = val_epoch(valid_loader)
+
+    content = time.ctime() + ' ' + f'Epoch {epoch}, lr: {optimizer.param_groups[0]["lr"]:.7f}, train loss: {np.mean(train_loss):.5f}, val loss: {np.mean(val_loss):.5f}, acc: {(acc):.5f}, qwk: {(qwk):.5f}'
+    print(content)
+    with open(f'log_{kernel_type}.txt', 'a') as appender:
+        appender.write(content + '\n')
+
+    if qwk > qwk_max:
+        print('score2 ({:.6f} --> {:.6f}).  Saving model ...'.format(qwk_max, qwk))
+        torch.save(model.state_dict(), best_file)
+        qwk_max = qwk
+
+torch.save(model.state_dict(), os.path.join(f'{kernel_type}_final_fold{fold}.pth'))
+
